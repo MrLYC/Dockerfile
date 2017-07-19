27a0e1fcfb59ac65a14d1d2efd8272936c2fd605
MrLYC <imyikong@gmail.com>
2017-07-20 01:39:10 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 set -x
 apk update
+apk info > /tmp/info1.txt
+
 apk add wget tar g++ make
 
 cp /usr/include/fcntl.h /usr/include/sys/fcntl.h
@@ -12,6 +14,9 @@ cd beanstalkd-1.10/
 make
 mv beanstalkd /beanstalkd
 
+apk info > /tmp/info2.txt
+
 rm -rf /var/cache/apk/*
 rm -f /build.sh || true
-apk info | grep -v apk | xargs -i apk del {} || true
+grep -F -f /tmp/info1.txt /tmp/info2.txt | xargs -i apk del {} || true
+rm -rf /tmp/*
