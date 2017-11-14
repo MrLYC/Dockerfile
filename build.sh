088f9dcc311201f86e9382e14a4bbfacf6ef019e
MrLYC <imyikong@gmail.com>
2017-11-14 17:46:01 +0000

    add phantomjs

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ apk update
 
 apk add g++ curl-dev libxml2-dev libxslt-dev openssl
 
+curl -Ls "https://github.com/dustinblackman/phantomized/releases/download/2.1.1a/dockerized-phantomjs.tar.gz" | tar xz -C /
+
 export PYCURL_SSL_LIBRARY=openssl
 pip install pyspider
 
