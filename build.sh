3e4003d5db1545645c517a61bc1aa58be039c043
MrLYC <imyikong@gmail.com>
2017-11-14 17:48:46 +0000

    add curl

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 set -x
 apk update
 
-apk add g++ curl-dev libxml2-dev libxslt-dev openssl
+apk add g++ curl curl-dev libxml2-dev libxslt-dev openssl
 
 curl -Ls "https://github.com/dustinblackman/phantomized/releases/download/2.1.1a/dockerized-phantomjs.tar.gz" | tar xz -C /
 
 export PYCURL_SSL_LIBRARY=openssl
 pip install pyspider
 
-apk del g++
+apk del g++ curl
 
 rm -rf /var/cache/apk/*
 rm -f /build.sh || true
