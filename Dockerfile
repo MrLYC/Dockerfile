7870448efd0efabe83b9fde4ddf0973e53d70825
MrLYC <imyikong@gmail.com>
2018-07-07 02:49:27 +0800

    add COMMUNITY env

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ ADD build.sh /build.sh
 ADD entry.sh /entry.sh
 
 ENV DOCKER_DEBUG 0
+ENV COMMUNITY mrlyc
 ENV LOG_LEVEL 0-3d
 ENV LOG_FILE snmpd.log
 ENV AGENT_ADDRESS udp:0.0.0.0:161
--- entry.sh
+++ entry.sh
@@ -7,6 +7,7 @@ fi
 if [ ! -r /data/snmpd.conf ]; then
     cat /etc/snmp/snmpd.conf | sed \
         -e "s/agentAddress  udp:127.0.0.1:161/agentAddress  ${AGENT_ADDRESS}/g" \
+        -e "s/rocommunity public  default    -V systemonly/rocommunity ${COMMUNITY}  default    -V systemonly/g" \
         ${CONFIG_SED_COMMAND} /etc/snmp/snmpd.conf | tee /data/snmpd.conf
 fi
 
