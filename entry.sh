e138d3065259bc2e6afc1b48995c28debf970358
MrLYC <imyikong@gmail.com>
2018-07-22 00:10:46 +0800

    remove pylab option

--- entry.sh
+++ entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [[ "${DOCKER_DEBUG}" == "1" ]]; then
+if [ "${DOCKER_DEBUG}" == "1" ]; then
     set -x
 fi
 
-/opt/conda/bin/python /opt/conda/bin/jupyter-notebook --pylab "enabled" --notebook-dir /home/jovyan/work
+/opt/conda/bin/python /opt/conda/bin/jupyter-notebook --notebook-dir /home/jovyan/work
