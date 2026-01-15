dac95aceaa160aa9889f2676df2f4da81f71c03b
MrLYC <imyikong@gmail.com>
2026-01-15 20:05:29 +0800

    feat(debian-dev): 新增 setup.sh 并更新 Dockerfile、mise.toml
    - 修改: images/debian-dev/Dockerfile, images/debian-dev/mise.toml
    - 新增: images/debian-dev/setup.sh

---
+++ images/debian-dev/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+opencode run -m opencode/glm-4.7-free "Install and configure by following the instructions here https://raw.githubusercontent.com/code-yeongyu/oh-my-opencode/refs/heads/master/README.md
+Note: I do not have any paid subscriptions and use only free models. Please handle everything and there is no need for further interaction with me."
